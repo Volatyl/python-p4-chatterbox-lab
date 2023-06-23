dfc198b6d6c6ef2172b23cddbde23cc8131166ad
Kennedy Muinde <kennedymuinde@gmail.com>
2023-06-23 04:14:27 +0300

    Finished lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,67 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+@app.route('/messages', methods=['GET', 'POST'])
+def messages():
+    if request.method == 'GET':
+        messages = Message.query.all()
+
+        msgs = []
+        for msg in messages:
+            msg_dict = msg.to_dict()
+            msgs.append(msg_dict)
+
+        res = make_response(jsonify(msgs), 200)
+
+        return res
+
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data.get('username')
+        body = data.get('body')
+
+        new_msg = Message(username=username, body=body)
+
+        db.session.add(new_msg)
+        db.session.commit()
+
+        msg_dict = new_msg.to_dict()
+
+        res = make_response(jsonify(msg_dict), 200)
+
+        return res
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    msg = Message.query.filter_by(id=id).first()
+
+    if request.method == 'PATCH':
+        # data = request.get_json()
+        for attr, value in request.json.items():
+            setattr(msg, attr, value)
+
+        db.session.commit()
+
+        res_body = msg.to_dict()
+
+        res = make_response(jsonify(res_body), 200)
+
+        return res
+
+    if request.method == 'DELETE':
+        db.session.delete(msg)
+        db.session.commit()
+
+        res_body = {
+            "delete": True,
+            "Delete msg": "Message deleted"
+        }
+
+        res = make_response(jsonify(res_body), 200)
+        return res
+
 
 if __name__ == '__main__':
     app.run(port=5555)
